c1af3cd8f02022b878df3f99e2d85a8bc117e155
Alex Kubiak <alex.kubiak10@gmail.com>
2018-05-30 06:25:26 -0400

    change to pset2_2-2

---
+++ week2/pset2_2-3.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 29 21:08:26 2018
+
+@author: Alex
+"""
+balance = 3329
+annualInterestRate = 0.2
+
+minimumPayment = 0
+localbalance = balance
+while localbalance >= 0:
+    localbalance = balance
+    minimumPayment += 10
+    localbalance = (localbalance*(1 + (annualInterestRate/12))**12) - (minimumPayment/(annualInterestRate/12))*(((1+(annualInterestRate/12))**12)-1)
+#    for month in range(12):
+#        localbalance = localbalance - minimumPayment
+#        interest = (annualInterestRate/12) * localbalance
+#        localbalance += interest   
+    print(str(minimumPayment) + " " + str(localbalance))
+print("Lowest Payment : " + str(minimumPayment))
